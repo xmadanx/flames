8e892aba1dd4e19bad4f126a3d4bbebde043936c
xmadanx <madanmn1742@gmail.com>
2025-08-16 18:07:20 +0530

    testing

--- app.py
+++ app.py
@@ -1,59 +1,58 @@
 def main():
-	name1 = input("1st Name: ").lower()
-	name2 = input("2nd Name: ").lower()
+    name1 = input("1st Name: ").lower()
+    name2 = input("2nd Name: ").lower()
 
-	namelist1 = list(name1)
-	namelist2 = list(name2)
- 
-	for i,j in enumerate(namelist1):
-		if j in namelist2:
-			print(i,j)
-			print(namelist2.index(j),namelist2[namelist2.index(j)])
-			namelist1[i] = "_"
-			namelist2[namelist2.index(j)] = "_"
+    namelist1 = list(name1)
+    namelist2 = list(name2)
 
-	namelist1.remove("_")
-	namelist2.remove("_")
+    for i,j in enumerate(namelist1):
+        if j in namelist2:
+            print(i,j)
+            print(namelist2.index(j),namelist2[namelist2.index(j)])
+            namelist1[i] = "_"
+            namelist2[namelist2.index(j)] = "_"
+    print(*namelist1)
+    namelist1 = list(filter(lambda x: x != "_", namelist1))
+    print(*namelist1)
+    print(*namelist2)
+    namelist2 = list(filter(lambda x: x != "_", namelist2))
+    print(*namelist2)
 
-	rvalue = len(namelist1) + len(namelist2)
+    rvalue = len(namelist1) + len(namelist2)
 
-	print(f"\nFLAMES value is {rvalue}\n")
+    print(f"\nFLAMES value is {rvalue}\n")
 
-	flist = list("FLAMES")
+    flist = list("FLAMES")
 
-	print(*flist)
+    print(*flist)
 
-	while len(flist) != 1:
-		if rvalue > len(flist):
-			value = rvalue % len(flist)
-		else:
-			value = rvalue
+    while len(flist) != 1:
+        if rvalue > len(flist):
+            value = rvalue % len(flist)
+        else:
+            value = rvalue
 
-		removed = flist.pop(value - 1)
+        removed = flist.pop(value - 1)
 
-		print(f"{removed} is removed")
+        print(f"{removed} is removed")
 
-		
 
-		for m in range(value-1):
-			flist.append(flist[0])
-			flist.pop(0)
-			
-		print(*flist)
-	flamesdict = {
-		"F" : "Friend",
-		"L" : "Love",
-		"A" : "Affection",
-		"M" : "Marriage",
-		"E" : "Enemy",
-		"S" : "Sister"
-	}
 
-	print(f"\n{flamesdict[flist[0]]}")
+        for m in range(value-1):
+            flist.append(flist[0])
+            flist.pop(0)
+
+        print(*flist)
+    flamesdict = {
+        "F" : "Friend",
+        "L" : "Love",
+        "A" : "Affection",
+        "M" : "Marriage",
+        "E" : "Enemy",
+        "S" : "Sister"
+    }
+
+    print(f"\n{flamesdict[flist[0]]}")
 
 if __name__ == "__main__":
-	main()
-
-
-
-
+    main()
